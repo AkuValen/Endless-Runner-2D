42719459e9a0f5e6ddd047da4265077582c62ce5
Valen <valentjulian@gmail.com>
2026-09-01 23:40:03 +0700

    feat: Menambahkan input type baru

--- src/core/input.js
+++ src/core/input.js
@@ -3,11 +3,15 @@ export class InputHandler {
     this.game = game;
 
     this.keys = [];
+    this.touch = {};
 
     this.isFocus = true;
+    this.isDrag = false;
 
     this.#keyInput();
+    this.#touchInput();
     this.#windowInput();
+    this.#documentInput();
   }
 
   #keyInput() {
@@ -26,6 +30,67 @@ export class InputHandler {
 
       if (index > -1) {
         const key = this.keys.splice(index, 1);
+      }
+    });
+  }
+
+  #touchInput() {
+    window.addEventListener(
+      "touchstart",
+      (e) => {
+        this.touch.startX = e.touches[0].clientX;
+        // this.touch.startY = e.touches[0].clientY;
+
+        this.isDrag = true;
+
+        console.log("touch: ", this.touch);
+      },
+      { passive: false },
+    );
+
+    window.addEventListener(
+      "touchmove",
+      (e) => {
+        e.preventDefault();
+
+        if (!this.isDrag) return;
+
+        let currentX = e.touches[0].clientX;
+
+        this.touch.currentX = currentX;
+
+        const directionX = Math.sign(this.touch.startX - currentX);
+
+        if (directionX < 0) {
+          console.log("Kanan");
+        }
+        if (directionX > 0) {
+          console.log("Kiri");
+        }
+
+        this.touch.startX = currentX;
+      },
+      { passive: false },
+    );
+
+    window.addEventListener("touchend", () => {
+      this.isDrag = false;
+
+      console.log("touch end");
+    });
+  }
+
+  #documentInput() {
+    document.addEventListener("visibilitychange", () => {
+      if (document.hidden) {
+        console.log("Pindah tab");
+
+        this.isFocus = false;
+      } else {
+        console.log("Kembali tab");
+
+        this.isFocus = true;
+        this.game.resume();
       }
     });
   }
